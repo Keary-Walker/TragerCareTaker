16ef7f427f6ed6db0d701e54fe3bcc6d50b6d53e
Keary Walker <klwalk12@gmail.com>
2021-09-10 12:15:58 -0400

    Added beginnings of UI for app redesign

--- TragerCareTaker/ContentView.swift
+++ TragerCareTaker/ContentView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        TabView {
+            
+            Text("5 Senses placeholder view")
+                .tabItem {
+                    Image(systemName: "book.circle")
+                    Text("5 Senses")
+                }
+            
+            Text("Resources placeholder view")
+                .tabItem {
+                    Image(systemName: "pencil.circle")
+                    Text("Resources")
+                }
+            
+            Text("Home placeholder view")
+                .tabItem {
+                    Image(systemName: "house.circle")
+                    Text("Home")
+                }
+            
+            Text("Self-care placeholder view")
+                .tabItem {
+                    Image(systemName: "cross.circle.fill")
+                    Text("Self-care")
+                }
+            
+            Text("Sounds placeholder view")
+                .tabItem {
+                    Image(systemName: "speaker.circle.fill")
+                    Text("Sounds")
+                }
+        }
+        
     }
 }
 
---
+++ TragerCareTaker/SymptomView.swift
@@ -0,0 +1,32 @@
+//
+//  SymptomView.swift
+//  SymptomView
+//
+//  Created by Keary Walker on 9/10/21.
+//
+
+import SwiftUI
+
+struct SymptomView: View {
+    
+    @State var title: String
+    @State var content: String
+    
+    var body: some View {
+        VStack() {
+            Text("\(title)")
+                .font(.title)
+                .padding()
+            VStack(alignment: .leading) {
+                Text("\(content)")
+            }
+            .padding()
+        }
+    }
+}
+
+struct SymptomView_Previews: PreviewProvider {
+    static var previews: some View {
+        SymptomView(title: "Hallucination", content: "When a person with Alzheimer's or other dementia hallucinates, they may see, hear, smell, taste or feel something that isn't there. Some hallucinations may be frightening, while others may involve ordinary visions of people, situations or objects from the past. Hallucinations are false perceptions of objects or events involving the senses. These false perceptions are caused by changes within the brain that result from Alzheimer’s, usually in the later stages of the disease. The person may see the face of a former friend in a curtain or may see insects crawling on his or her hand. In other cases, a person may hear someone talking and may even engage in conversation with the imagined person.")
+    }
+}
