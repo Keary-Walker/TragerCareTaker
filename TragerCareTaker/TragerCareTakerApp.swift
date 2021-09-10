89a94ba7aff9bed20eeb1cc702bc37b4af58beef
Keary Walker <klwalk12@gmail.com>
2021-09-10 11:33:47 -0400

    Initial Commit

---
+++ TragerCareTaker/TragerCareTakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TragerCareTakerApp.swift
+//  TragerCareTaker
+//
+//  Created by Keary Walker on 9/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TragerCareTakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
