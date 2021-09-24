432ddf7caf1d8baaf0688033669e6fde154c907b
Keary Walker <klwalk12@gmail.com>
2021-09-24 00:35:45 -0400

    HomeView added. The initial design matches what was given to us by Sam although upon further inspection it may need to be changed. None of the home tab's UI is function it is purely aesthetic.

--- TragerCareTaker/ContentView.swift
+++ TragerCareTaker/ContentView.swift
@@ -10,37 +10,39 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        TabView {
-            
-            Text("5 Senses placeholder view")
-                .tabItem {
-                    Image(systemName: "book.circle")
-                    Text("5 Senses")
-                }
-            
-            Text("Resources placeholder view")
-                .tabItem {
-                    Image(systemName: "pencil.circle")
-                    Text("Resources")
-                }
-            
-            Text("Home placeholder view")
-                .tabItem {
-                    Image(systemName: "house.circle")
-                    Text("Home")
-                }
-            
-            Text("Self-care placeholder view")
-                .tabItem {
-                    Image(systemName: "cross.circle.fill")
-                    Text("Self-care")
-                }
-            
-            Text("Sounds placeholder view")
-                .tabItem {
-                    Image(systemName: "speaker.circle.fill")
-                    Text("Sounds")
-                }
+        VStack {
+            TabView {
+                
+                Text("5 Senses placeholder view")
+                    .tabItem {
+                        Image(systemName: "book.circle")
+                        Text("5 Senses")
+                    }
+                
+                Text("Resources placeholder view")
+                    .tabItem {
+                        Image(systemName: "pencil.circle")
+                        Text("Resources")
+                    }
+                
+                HomeView()
+                    .tabItem {
+                        Image(systemName: "house.circle")
+                        Text("Home")
+                    }
+                
+                Text("Self-care placeholder view")
+                    .tabItem {
+                        Image(systemName: "cross.circle.fill")
+                        Text("Self-care")
+                    }
+                
+                Text("Sounds placeholder view")
+                    .tabItem {
+                        Image(systemName: "speaker.circle.fill")
+                        Text("Sounds")
+                    }
+            }
         }
         
     }
---
+++ TragerCareTaker/HomeView.swift
@@ -0,0 +1,112 @@
+//
+//  SwiftUIView.swift
+//  TragerCareTaker
+//
+//  Created by Keary Walker on 9/23/21.
+//
+
+import SwiftUI
+
+struct BlueButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(width:30, height: 30)
+            .padding()
+            .background(Color(red: 0.01, green: 0.48, blue: 1))
+            .foregroundColor(.white)
+            .clipShape(Circle())
+    }
+}
+
+struct HomeView: View {
+    @State private var searchContent: String = ""
+    var body: some View {
+        VStack {
+            
+            HStack {
+                Text("Search:")
+                
+                TextField("", text: $searchContent)
+                    .textFieldStyle(.roundedBorder)
+                Button("Confirm") {
+                    print("Implement search")
+                }
+            }
+            .padding()
+            
+            HStack(spacing: 30) {
+                VStack {
+                    Button("") {
+                        print("Button pressed!")
+                    }
+                    .buttonStyle(BlueButton())
+                    Text("My Note")
+                }
+                
+                VStack {
+                    Button("") {
+                        print("Button pressed!")
+                    }
+                    .buttonStyle(BlueButton())
+                    Text("Videos")
+                }
+                
+                VStack {
+                    Button("") {
+                        print("Button pressed!")
+                    }
+                    .buttonStyle(BlueButton())
+                    Text("Behavior")
+                }
+                
+                VStack {
+                    Button("") {
+                        print("Button pressed!")
+                    }
+                    .buttonStyle(BlueButton())
+                    Text("Glossary")
+                }
+            }
+            
+            VStack {
+                HStack {
+                    Text("Today's Task:")
+                    Spacer()
+                    Button("Delete"){
+                        print("something")
+                    }
+                }
+                .padding()
+            }
+            
+            VStack {
+                HStack {
+                    Text("Alert:")
+                    Spacer()
+                    Button("Delete"){
+                        print("something")
+                    }
+                }
+                .padding()
+            }
+            
+
+            HStack {
+                Text("Self-care Tip:")
+                Spacer()
+                Button("Delete"){
+                    print("something")
+                }
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
